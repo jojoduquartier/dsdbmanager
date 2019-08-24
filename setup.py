f510843a23f10a0e059cf9ea73a79b8c67d9194a
Josiah Hounyo <josiahhounyo@gmail.com>
2019-08-24 13:05:29 -0500

    entry point

--- setup.py
+++ setup.py
@@ -139,11 +139,11 @@ setup(
     #
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
-    # entry_points={  # Optional
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
+    entry_points={  # Optional
+        'console_scripts': [
+            'dsdbmanager=dsdbmanager.cli:main',
+        ],
+    },
 
     # List additional URLs that are relevant to your project as a dict.
     #
